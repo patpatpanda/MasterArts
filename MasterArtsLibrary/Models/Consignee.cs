cc23179fb911fdecc31f80b50b672137a5dedc79
patpatpanda <nils-emil1337@hotmail.se>
2024-02-08 16:03:47 +0100

    a

--- MasterArtsLibrary/Models/Consignee.cs
+++ MasterArtsLibrary/Models/Consignee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,8 @@ namespace MasterArtsLibrary.Models
         public int Id { get; set; }
         public string ConsigneeName { get; set; }
         public string ConsigneeAddress1 { get; set; }
-        public string ConsigneeAddress2 { get; set; }
+        [Display(Name = "Consignee Address 2")]
+        public string? ConsigneeAddress2 { get; set; }
         public string ConsigneeCity { get; set; }
         public string ConsigneeCountry { get; set; }
         public string ConsigneeZip { get; set; }
